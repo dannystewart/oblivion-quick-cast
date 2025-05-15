45ef80e1a85efd0b40ce18ac55ff9019e4fc3640
Danny Stewart <danny@stewart.cc>
2025-05-15 18:21:39 -0400

    Add nested ExecuteInGameThread for additional frame delay
    Addresses race condition when switching spells by adding a second nested ExecuteInGameThread call to ensure spell casting happens at least 2 frames after weapon switching.
    
    Updates README to indicate this is still a work in progress and not fully reliable yet.

--- OblivionRemastered/Binaries/Win64/ue4ss/Mods/QuickCast/Scripts/main.lua
+++ OblivionRemastered/Binaries/Win64/ue4ss/Mods/QuickCast/Scripts/main.lua
@@ -26,32 +26,35 @@ local function CastSpellWithQuickInput(slot)
     end
 
     ExecuteInGameThread(function()
-        local freshQuickMenu = FindFirstOf("VQuickKeysMenuViewModel")
-        if not freshQuickMenu then
-            log("QuickMenuModel is null/nil in delayed execution")
-            return
-        end
-
-        local slotIconFullName = freshQuickMenu.Icons[slot]:GetFullName()
-        if not slotIconFullName then
-            log("SlotIconFullName is nil or empty")
-            return
-        end
-
-        if string.find(slotIconFullName, "icons%/magic%/") then
-            log("String matched: " .. slotIconFullName)
-
-            local playerController = UEHelpers.GetPlayerController()
-            if not playerController or not playerController:IsValid() then
-                log("PlayerController not found or doesn't exist.")
+        -- Nested ExecuteInGameThread calls to ensure we're at least 2 frames after the weapon switch
+        ExecuteInGameThread(function()
+            local freshQuickMenu = FindFirstOf("VQuickKeysMenuViewModel")
+            if not freshQuickMenu then
+                log("QuickMenuModel is null/nil in delayed execution")
                 return
             end
 
-            log("Casting with QuickInput")
-            playerController:ToggleSpellCastPressed()
-        else
-            log("String not found")
-        end
+            local slotIconFullName = freshQuickMenu.Icons[slot]:GetFullName()
+            if not slotIconFullName then
+                log("SlotIconFullName is nil or empty")
+                return
+            end
+
+            if string.find(slotIconFullName, "icons%/magic%/") then
+                log("String matched: " .. slotIconFullName)
+
+                local playerController = UEHelpers.GetPlayerController()
+                if not playerController or not playerController:IsValid() then
+                    log("PlayerController not found or doesn't exist.")
+                    return
+                end
+
+                log("Casting with QuickInput")
+                playerController:ToggleSpellCastPressed()
+            else
+                log("String not found")
+            end
+        end)
     end)
 end
 
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This is a mod of a mod for Oblivion Remastered by [Nistaux](https://www.nexusmods.com/oblivionremastered/users/21571864) on [Nexus Mods](https://www.nexusmods.com/oblivionremastered/mods/1531). I had an issue which no one else seems to be experiencing based on the mod posts where when I pressed the number hotkeys (1-8), there was often a race condition where it was casting the current spell before switching to the new spell. If I was on 1 and pressed 2, sometimes 1 would cast because it hadn't switched to 2 yet.
 
-I don't know much about Lua, Unreal Engine 5, or UE4SS modding, but I tried my hand at fixing it, and in initial testing, I haven't seen the issue since. I hope maybe this helps someone else too.
+I don't know much about Lua, Unreal Engine 5, or UE4SS modding, but I'm trying my hand at fixing it. This is still a work in progress and not 100% reliable yet.
 
 To install, you need [UE4SS](https://www.nexusmods.com/oblivionremastered/mods/32), and the mod should go here: `OblivionRemastered\Binaries\Win64\ue4ss\Mods\QuickCast\Scripts\main.lua`
